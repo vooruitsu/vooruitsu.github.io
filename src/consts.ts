c9644508b6e1b8d146588a1c457d49c108494950
p-febis <preston.febis@yahoo.com>
2025-11-02 19:25:31 -0300

    Added favicon, fixed workflow

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Vooruit!";
+export const SITE_DESCRIPTION = "Vooruit! is een revolutionaire en vooruitstrevende krant met als bedoeling het klassenbewustzijn in Suriname te vergroten.";
